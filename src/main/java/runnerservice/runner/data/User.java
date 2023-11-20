557849b0ba1b9d4ce66d2a2601283b14982e9c5c
schikimikiviki <viktoria.bzdyk@gmx.at>
2023-11-20 23:12:56 +0100

    basic configuration

--- src/main/java/runnerservice/runner/data/User.java
+++ src/main/java/runnerservice/runner/data/User.java
@@ -3,8 +3,10 @@ package runnerservice.runner.data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user_table")
 public class User {
     @Id
     @GeneratedValue
